8667c87b6a85b9494237a7075f6b0569f1c8beca
phongnd <phongnguyendx@gmail.com>
2023-08-25 16:24:56 +0700

    done

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.get('/blob', (req, res) => {
 const Videos = require('./routes/Videos')
 app.use('/videos', Videos)
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Listening on port 5000!')
 });
 
